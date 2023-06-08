7800c2f84e98c2a751f82ca66e607f8365c88b9f
FeGem99 <f.gemma@studenti.uniba.it>
2023-06-08 19:20:59 +0200

    Aggiunti animazioni base
    Aggiunto moviemento da fermo dardust e zelena, con corsa al momento solo a durdust

--- Assets/Dardust_Movement.cs
+++ Assets/Dardust_Movement.cs
@@ -7,6 +7,9 @@ public class Dardust_Movement : MonoBehaviour
 
    private Rigidbody2D dardust;
    private bool isJumping = false;
+
+private float dirX = 0f; //variabile che indica la direzione dove si muove durdust, settata a 0.
+   private Animator anim; //variabile per animazione
    /* int wholeNumber = 16;
     float decimalNumber = 4.54f;
     string text = "this->";
@@ -15,24 +18,39 @@ public class Dardust_Movement : MonoBehaviour
    private void Start()
     {
        dardust =  GetComponent<Rigidbody2D>();
+       anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void Update()
     {
 
-        float dirX = Input.GetAxisRaw("Horizontal");
+        dirX = Input.GetAxisRaw("Horizontal");
         dardust.velocity = new Vector2(dirX * 3.5f, dardust.velocity.y);
        if (Input.GetButtonDown("Jump")&& !isJumping)
        {
       dardust.velocity = new Vector3(dardust.velocity.x, 5.5f);
       isJumping = true;
       } 
-      
 
+      UpdateAnimationUpdate();
 
     }
 
+private void UpdateAnimationUpdate(){ //metodo che controlla le animazioni di durdust 
+
+if (dirX > 0f)
+      {
+          anim.SetBool("Running_D", true);
+      }
+      else if (dirX < 0f)
+      {
+          anim.SetBool("Running_D", true);
+      }
+      else {
+          anim.SetBool("Running_D", false); 
+      }
+}
     private void OnCollisionEnter2D(Collision2D collision){
         if(collision.gameObject.CompareTag("Terrain")){
             isJumping = false;
