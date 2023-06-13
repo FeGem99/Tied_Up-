0047f78b9f1fb2573de13290bfd18082a910a967
emanuele231 <e.destena1@studenti.uniba.it>
2023-06-13 18:26:29 +0200

    corda presente
    impiccatevi

--- Assets/Corda.cs
+++ Assets/Corda.cs
@@ -4,33 +4,57 @@ using UnityEngine;
 
 public class Corda : MonoBehaviour
 {
-     public Transform personaggio1;
+    public Transform personaggio1;
     public Transform personaggio2;
-    public float lunghezzaMassima = 5f;
+    public float lunghezzaMassima;
+    public float rigidita = 5f;
 
     private LineRenderer lineRenderer;
-  
-  
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rbPersonaggio1;
+    private Rigidbody2D rbPersonaggio2;
+    private Vector2 direzioneIniziale;
+
+    private void Start()
     {
-     lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.positionCount = 2;   
+        lineRenderer = GetComponent<LineRenderer>();
+        rbPersonaggio1 = personaggio1.GetComponent<Rigidbody2D>();
+        rbPersonaggio2 = personaggio2.GetComponent<Rigidbody2D>();
+
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPosition(0, personaggio1.position);
+        lineRenderer.SetPosition(1, personaggio2.position);
+
+        direzioneIniziale = rbPersonaggio2.position - rbPersonaggio1.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-      Vector3 posizione1 = personaggio1.position;
-        Vector3 posizione2 = personaggio2.position;
+        Vector2 posizione1 = rbPersonaggio1.position;
+        Vector2 posizione2 = rbPersonaggio2.position;
 
-        float distanza = Vector3.Distance(posizione1, posizione2);
-        float lunghezzaAttuale = Mathf.Min(distanza, lunghezzaMassima);
+        Vector2 direzioneAttuale = posizione2 - posizione1;
+        float distanzaAttuale = direzioneAttuale.magnitude;
 
-        Vector3 direzione = (posizione2 - posizione1).normalized;
-        Vector3 posizioneFine = posizione1 + direzione * lunghezzaAttuale;
+        if (distanzaAttuale > lunghezzaMassima)
+        {
+            direzioneAttuale = direzioneAttuale.normalized * lunghezzaMassima;
+            posizione2 = posizione1 + direzioneAttuale;
+            rbPersonaggio2.position = posizione2;
+        }
 
         lineRenderer.SetPosition(0, posizione1);
-        lineRenderer.SetPosition(1, posizioneFine);
+        lineRenderer.SetPosition(1, posizione2);
+
+        // Applica una forza per mantenere la distanza iniziale tra i personaggi
+        Vector2 forza = rigidita * (direzioneIniziale - direzioneAttuale);
+        rbPersonaggio1.AddForce(forza);
+        rbPersonaggio2.AddForce(-forza);
     }
 }
+
+
+
+
+
+
+
