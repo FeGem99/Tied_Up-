7cad8fac0ef17544a7b161084a7427f92ed1393a
FeGem99 <f.gemma@studenti.uniba.it>
2023-06-13 17:51:53 +0200

    Ottimizzati movimenti dardust e la puttana
    Ho sistemato lo script di entrambi i personaggi, e ho sistemato il box collider del terreno.

--- Assets/Dardust_Movement.cs
+++ Assets/Dardust_Movement.cs
@@ -6,10 +6,12 @@ public class Dardust_Movement : MonoBehaviour
 {
 
    private Rigidbody2D dardust;
+   private BoxCollider2D coll;
    private bool isJumping = false;
     private SpriteRenderer sprite;
 private float dirX = 0f; //variabile che indica la direzione dove si muove durdust, settata a 0.
    private Animator anim; //variabile per animazione
+   [SerializeField] private LayerMask JumpableGround;
    /* int wholeNumber = 16;
     float decimalNumber = 4.54f;
     string text = "this->";
@@ -20,6 +22,7 @@ private float dirX = 0f; //variabile che indica la direzione dove si muove durdu
        dardust =  GetComponent<Rigidbody2D>();
        anim = GetComponent<Animator>();
        sprite = GetComponent<SpriteRenderer>();
+       coll = GetComponent<BoxCollider2D>(); 
     }
 
     // Update is called once per frame
@@ -28,7 +31,7 @@ private float dirX = 0f; //variabile che indica la direzione dove si muove durdu
 
         dirX = Input.GetAxisRaw("Horizontal");
         dardust.velocity = new Vector2(dirX * 3.5f, dardust.velocity.y);
-       if (Input.GetButtonDown("Jump")&& !isJumping)
+       if (Input.GetButtonDown("Jump")&& isGrounded())
        {
       dardust.velocity = new Vector3(dardust.velocity.x, 5.5f);
       isJumping = true;
@@ -59,6 +62,9 @@ if (dirX > 0f)
         if(collision.gameObject.CompareTag("Terrain")){
             isJumping = false;
         }
+    }
+    private bool isGrounded(){
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, JumpableGround);
     }
 }
 
