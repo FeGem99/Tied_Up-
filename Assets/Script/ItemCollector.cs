38353961ab4d47e18679784e99d7a1dda980613c
FeGem99 <f.gemma@studenti.uniba.it>
2023-06-28 22:26:07 +0200

    Siamo pronti al merge finale
    Ho sistemato e aggiunto a tutte le scene, il conteggio del punteggio, le gemme collezionabili e il counter degli items. Ora Speriamo dio che funzioni tutto

--- Assets/Script/ItemCollector.cs
+++ Assets/Script/ItemCollector.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ItemCollector : MonoBehaviour
 {
+    [SerializeField] private Text GemsText;
+    private int gemme = 0;
     private void OnTriggerEnter2D (Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Gems")){
             Destroy(collision.gameObject);
+            gemme++;
+            GemsText.text = "Gemme: " +gemme;
         }
     }
 }
