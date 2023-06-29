519e3d20d32226f5481b3cb5b296b3f33b40f527
FeGem99 <f.gemma@studenti.uniba.it>
2023-06-29 21:28:20 +0200

    Il god supremo
    Aggiornato in tutti i livelli Durdust e zelena che si possono saltare. In più sistemato in ogni livello la porcaccia madonna di barra superiore. Ora è uguale e funziona in tutti i livelli.

--- Assets/Script/Dardust_Movement.cs
+++ Assets/Script/Dardust_Movement.cs
@@ -4,67 +4,73 @@ using UnityEngine;
 
 public class Dardust_Movement : MonoBehaviour
 {
-
-   private Rigidbody2D dardust;
-   private BoxCollider2D coll;
-   private bool isJumping = false;
+    private Rigidbody2D dardust;
+    private BoxCollider2D coll;
+    private bool isJumping = false;
     private SpriteRenderer sprite;
-private float dirX = 0f; //variabile che indica la direzione dove si muove durdust, settata a 0.
-   private Animator anim; //variabile per animazione
-   [SerializeField] private LayerMask JumpableGround;
-   /* int wholeNumber = 16;
-    float decimalNumber = 4.54f;
-    string text = "this->";
-    bool boolean = false;*/
-    // Start is called before the first frame update
-   private void Start()
+    private float dirX = 0f;
+    private Animator anim;
+    [SerializeField] private LayerMask JumpableGround;
+     [SerializeField] private LayerMask JumpableObstacle;
+    private void Start()
     {
-       dardust =  GetComponent<Rigidbody2D>();
-       anim = GetComponent<Animator>();
-       sprite = GetComponent<SpriteRenderer>();
-       coll = GetComponent<BoxCollider2D>(); 
+        dardust = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
     private void Update()
     {
-
         dirX = Input.GetAxisRaw("Horizontal");
         dardust.velocity = new Vector2(dirX * 3.5f, dardust.velocity.y);
-       if (Input.GetButtonDown("Jump")&& isGrounded())
-       {
-      dardust.velocity = new Vector3(dardust.velocity.x, 5.5f);
-      isJumping = true;
-      } 
 
-      UpdateAnimationUpdate();
+        if (Input.GetButtonDown("Jump") && (isGrounded() || isObstacleColliding()))
+        {
+            dardust.velocity = new Vector2(dardust.velocity.x, 5.5f);
+            isJumping = true;
+        }
 
+        UpdateAnimation();
     }
 
-private void UpdateAnimationUpdate(){ //metodo che controlla le animazioni di durdust 
+    private void UpdateAnimation()
+    {
+        if (dirX > 0f)
+        {
+            anim.SetBool("run_D", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("run_D", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("run_D", false);
+        }
+    }
 
-if (dirX > 0f)
-      {
-          anim.SetBool("run_D", true);
-          sprite.flipX = false;
-      }
-      else if (dirX < 0f)
-      {
-          anim.SetBool("run_D", true);
-          sprite.flipX = true;
-      }
-      else {
-          anim.SetBool("run_D", false); 
-         
-      }
-}
-    private void OnCollisionEnter2D(Collision2D collision){
-        if(collision.gameObject.CompareTag("Terrain")){
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Terrain"))
+        {
             isJumping = false;
         }
     }
-    private bool isGrounded(){
-        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, JumpableGround);
+
+    private bool isGrounded()
+    {
+        float extraHeight = 0.1f;
+        RaycastHit2D raycastHit = Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, extraHeight, JumpableGround);
+        return raycastHit.collider != null;
+    }
+
+    private bool isObstacleColliding()
+    {
+        float extraHeight = 0.1f;
+        RaycastHit2D raycastHit = Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, extraHeight, JumpableObstacle);
+        return raycastHit.collider != null;
     }
 }
-
--- Assets/Script/Zelena_Movement.cs
+++ Assets/Script/Zelena_Movement.cs
@@ -4,71 +4,75 @@ using UnityEngine;
 
 public class Zelena_Movement : MonoBehaviour
 {
+    private Rigidbody2D zelena;
+    private BoxCollider2D coll; 
+    private bool isJumping = false;
+    private float dirX = 0f; 
+    private SpriteRenderer sprite;
+    private Animator anim;
 
-   private Rigidbody2D zelena;
-   private BoxCollider2D coll; 
-   private bool isJumping = false;
-   private float dirX = 0f; 
-   private SpriteRenderer sprite;
- private Animator anim; //variabile per animazione
+    [SerializeField] private LayerMask JumpableGround;
+    [SerializeField] private LayerMask JumpableObstacles;
 
- [SerializeField] private LayerMask JumpableGround; 
-   /* int wholeNumber = 16;
-    float decimalNumber = 4.54f;
-    string text = "this->";
-    bool boolean = false;*/
-    // Start is called before the first frame update
-   private void Start()
+    private void Start()
     {
-       zelena =  GetComponent<Rigidbody2D>();
-       coll = GetComponent<BoxCollider2D>();
+        zelena = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
-       //int jumpD = 0;
     }
 
-    // Update is called once per frame
     private void Update()
     {
-         
         dirX = Input.GetAxisRaw("HorizontalZ");
         zelena.velocity = new Vector2(dirX * 3.5f, zelena.velocity.y);
-       if (Input.GetButtonDown("JumpZ") && isGrounded())
-       {
-        
-      zelena.velocity = new Vector3(zelena.velocity.x, 5.5f);
-      isJumping=true;
-      } 
-       UpdateAnimationUpdate();
 
-       
+        if (Input.GetButtonDown("JumpZ") && (isGrounded() || isObstacleColliding()))
+        {
+            zelena.velocity = new Vector2(zelena.velocity.x, 5.5f);
+            isJumping = true;
+        }
+
+        UpdateAnimation();
     }
-    private void UpdateAnimationUpdate(){ //metodo che controlla le animazioni di durdust 
 
-if (dirX > 0f)
-      {
-          anim.SetBool("run_Z", true);
-          sprite.flipX = false;
-      }
-      else if (dirX < 0f)
-      {
-          anim.SetBool("run_Z", true);
-          sprite.flipX = true;
-      }
-      else {
-          anim.SetBool("run_Z", false); 
+    private void UpdateAnimation()
+    {
+        if (dirX > 0f)
+        {
+            anim.SetBool("run_Z", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("run_Z", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("run_Z", false); 
+        }
+    }
 
-      }
-}
-      private void OnCollisionEnter2D(Collision2D collision){
-        if(collision.gameObject.CompareTag("Terrain")){
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Terrain"))
+        {
             isJumping = false;
         }
     }
-    private bool isGrounded(){
-       return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, JumpableGround);
 
+    private bool isGrounded()
+    {
+        float extraHeight = 0.1f;
+        RaycastHit2D raycastHit = Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, extraHeight, JumpableGround);
+        return raycastHit.collider != null;
+    }
+
+    private bool isObstacleColliding()
+    {
+        float extraHeight = 0.1f;
+        RaycastHit2D raycastHit = Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, extraHeight, JumpableObstacles);
+        return raycastHit.collider != null;
     }
 }
-
-
