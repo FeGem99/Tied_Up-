396909525dbfe2ff31a798cadb38395dd9e967c3
emanuele231 <e.destena1@studenti.uniba.it>
2023-07-02 17:55:37 +0200

    inserite scene prima del livello
    ma e da sistemare

---
+++ Assets/Script/liv1Start.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class liv1Start : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("intro-livello1");
+    }
+}
---
+++ Assets/Script/liv3Start.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class liv3Start : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("intro-livello3");
+    }
+}
