a48fe3f17666904454c7a4d4b8a9f9a4d712293d
emanuele231 <e.destena1@studenti.uniba.it>
2023-07-03 16:08:47 +0200

    aggiunto uscita del livello e risolto il bug delle gemme

--- Assets/Script/ItemCollector.cs
+++ Assets/Script/ItemCollector.cs
@@ -6,15 +6,27 @@ public class ItemCollector : MonoBehaviour
 {
     [SerializeField] private Text GemsText;
     public int gemme = 0;
+    public int gemmeTotali = 0;
+    public GameObject personaggio1; // Riferimento al primo personaggio
+    public GameObject personaggio2; // Riferimento al secondo personag
     private void OnTriggerEnter2D (Collider2D collision)
     {
+        ItemCollector personaggio1Collector = personaggio1.GetComponent<ItemCollector>();
+
+            // Ottieni il componente ItemCollector dal secondo personaggio
+            ItemCollector personaggio2Collector = personaggio2.GetComponent<ItemCollector>();
+
+            // Somma le gemme raccolte da entrambi i personaggi
+            //int gemmeTotali = personaggio1Collector.gemme + personaggio2Collector.gemme;
         if(collision.gameObject.CompareTag("Gems")){
             Destroy(collision.gameObject);
             gemme++;
-            GemsText.text = "Gemme: " +gemme;
+        
+             gemmeTotali = personaggio1Collector.gemme + personaggio2Collector.gemme;
+            GemsText.text = "Gemme: " +gemmeTotali;
 
             //aggiunta logica della fine del livello
-            if (gemme == 3)
+            if (gemmeTotali == 3)
             {
                 GameObject[] sbarre = GameObject.FindGameObjectsWithTag("sbarra");
 
