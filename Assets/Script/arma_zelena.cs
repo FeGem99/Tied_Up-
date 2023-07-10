de7cced2d2f54bb5d73d8a4d961c0e02ca67faf5
emanuele231 <e.destena1@studenti.uniba.it>
2023-07-10 15:58:38 +0200

    aggiunto attacco di zelena

--- Assets/Script/arma_zelena.cs
+++ Assets/Script/arma_zelena.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using UnityEngine;
+
 
 public class arma_zelena : MonoBehaviour
 {
@@ -10,9 +10,9 @@ public class arma_zelena : MonoBehaviour
     public Transform firePoint; // Punto di origine del proiettile
     public KeyCode fireKey = KeyCode.DownArrow; // Tasto per attivare l'azione dell'arma
 
-    private bool canFire = true; // Indica se l'arma puÚ sparare
+    private bool canFire = true; // Indica se l'arma pu√≤ sparare
 
-    void Update()
+    private void Update()
     {
         if (Input.GetKeyDown(fireKey) && canFire)
         {
@@ -20,25 +20,28 @@ public class arma_zelena : MonoBehaviour
         }
     }
 
-    void Fire()
+    private void Fire()
     {
         // Crea un nuovo proiettile a partire dal prefab
         GameObject projectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
 
+        // Determina la direzione del personaggio in base all'input di movimento
+        float direction = Input.GetAxisRaw("HorizontalZ");
+
         // Esegui eventuali altre azioni dell'arma, come l'animazione dell'attacco
 
-        // Esempio: applica una forza al proiettile in avanti
+        // Esempio: applica una forza al proiettile nella direzione corretta
         Rigidbody2D projectileRigidbody = projectile.GetComponent<Rigidbody2D>();
-        projectileRigidbody.AddForce(firePoint.right * 500f);
+        projectileRigidbody.AddForce(Vector2.right * direction * 500f);
 
-        // Disabilita temporaneamente la possibilit‡ di sparare per evitare il fuoco continuo
+        // Disabilita temporaneamente la possibilit√† di sparare per evitare il fuoco continuo
         canFire = false;
 
         // Avvia una coroutine per riabilitare l'uso dell'arma dopo un certo intervallo di tempo
         StartCoroutine(ResetFire());
     }
 
-    IEnumerator ResetFire()
+    private IEnumerator ResetFire()
     {
         // Attendi per un certo intervallo di tempo prima di riabilitare l'uso dell'arma
         yield return new WaitForSeconds(1f); // Modifica il valore se desideri un intervallo diverso
@@ -46,5 +49,7 @@ public class arma_zelena : MonoBehaviour
         // Riabilita l'uso dell'arma
         canFire = true;
     }
-}
+ }
+
+
 
