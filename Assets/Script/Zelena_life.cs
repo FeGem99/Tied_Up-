182bbe70c16a9bf10f460e14bb988e93b4b0b569
emanuele231 <e.destena1@studenti.uniba.it>
2023-07-11 12:59:51 +0200

    inserita vita, morte per trappole e strangolamento
    manca l'animazione della morte

--- Assets/Script/Zelena_life.cs
+++ Assets/Script/Zelena_life.cs
@@ -2,34 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement; 
+using UnityEngine.UI;
 public class Zelena_life : MonoBehaviour
 {
-    private Animator anim; 
+      private Animator anim;
     private Rigidbody2D rb;
-    private score_progress score; 
+    private score_progress score;
+    public Text lifeText; // Riferimento al testo UI della vita del personaggio
+    public int maxLife = 100; // Vita massima del personaggio
+    private int currentLife; // Vita attuale del personaggio
+
     private void Start()
     {
         score = GetComponent<score_progress>();
-        anim = GetComponent <Animator>();
-        rb = GetComponent <Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        currentLife = maxLife;
+        UpdateLifeText();
     }
-    
-    private void OnCollisionEnter2D (Collision2D collision){
-        if (collision.gameObject.CompareTag("trap")){
-            Die(); 
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("trap"))
+        {
+            TakeDamage(100);
         }
-        
     }
-private void Die()
-{
-    rb.bodyType = RigidbodyType2D.Static; 
-    anim.SetTrigger("Death");
 
-}
-  private void RestartLevel()
- {
-     SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
-     score.setDelay();
- }
+    private void TakeDamage(int damage)
+    {
+        currentLife -= damage;
+        UpdateLifeText();
 
+        if (currentLife <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+        anim.SetTrigger("Death");
+
+        // Puoi aggiungere qui altre azioni quando il personaggio muore, come mostrare un pannello di Game Over, ecc.
+    }
+
+    private void UpdateLifeText()
+    {
+        lifeText.text = " " + currentLife.ToString();
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        score.setDelay();
+    }
 }
